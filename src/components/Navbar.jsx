59913652de394b064938a1866690e2c53d1ac797
TomChuck <m.tomczyk@gymmaster.io>
2025-02-26 00:36:22 +0100

    Completed base of the page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,95 @@
+import { useState, useEffect } from "react";
+import ViteIcon from "../assets/react.svg";
+
+const Navbar = () => {
+	const [activeSection, setActiveSection] = useState("");
+
+	const scrollToSection = sectionId => {
+		const element = document.getElementById(sectionId);
+		if (element) {
+			element.scrollIntoView({ behavior: "smooth" });
+			setActiveSection(sectionId);
+		}
+	};
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const sections = ["about", "pricing", "barbers", "works"];
+			const scrollPosition = window.scrollY + window.innerHeight / 2;
+
+			for (let section of sections) {
+				const element = document.getElementById(section);
+				if (
+					element &&
+					element.offsetTop <= scrollPosition &&
+					element.offsetTop + element.offsetHeight > scrollPosition
+				) {
+					setActiveSection(section);
+					break;
+				}
+			}
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
+	return (
+		<nav className='flex justify-between items-center w-full fixed top-4 left-0 bg-transparent px-20 py-4'>
+			<div className='flex space-x-10 uppercase'>
+				<p
+					className={`text-textPrimary cursor-pointer flex items-center ${
+						activeSection === "about" ? "text-orange-600" : ""
+					}`}
+					onClick={() => scrollToSection("about")}>
+					{activeSection === "about" && (
+						<span className='w-2 h-2 bg-orange-600 rounded-full mr-2'></span>
+					)}
+					o nas
+				</p>
+				<p
+					className={`text-textPrimary cursor-pointer flex items-center ${
+						activeSection === "pricing" ? "text-orange-600" : ""
+					}`}
+					onClick={() => scrollToSection("pricing")}>
+					{activeSection === "pricing" && (
+						<span className='w-2 h-2 bg-orange-600 rounded-full mr-2'></span>
+					)}
+					cennik
+				</p>
+				<p
+					className={`text-textPrimary cursor-pointer flex items-center ${
+						activeSection === "barbers" ? "text-orange-600" : ""
+					}`}
+					onClick={() => scrollToSection("barbers")}>
+					{activeSection === "barbers" && (
+						<span className='w-2 h-2 bg-orange-600 rounded-full mr-2'></span>
+					)}
+					barberzy
+				</p>
+				<p
+					className={`text-textPrimary cursor-pointer flex items-center ${
+						activeSection === "works" ? "text-orange-600" : ""
+					}`}
+					onClick={() => scrollToSection("works")}>
+					{activeSection === "works" && (
+						<span className='w-2 h-2 bg-orange-600 rounded-full mr-2'></span>
+					)}
+					nasze prace
+				</p>
+			</div>
+			<div className='flex justify-center absolute left-1/2 transform -translate-x-1/2'>
+				<img src={ViteIcon} alt='Vite Icon' />
+			</div>
+			<div className='flex space-x-10 uppercase'>
+				<p className='text-textPrimary'>blog</p>
+				<p className='text-textPrimary'>szkolenia</p>
+				<p className='text-textPrimary'>zarezerwuj</p>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,53 +1,140 @@
-import ViteIcon from "../assets/react.svg";
+import Navbar from "../components/Navbar";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const HomePage = () => {
-  return (
-    <div className="flex flex-col min-h-screen px-20 pt-12">
-      <nav className="flex justify-between items-center w-full relative">
-        <div className="flex space-x-10 uppercase">
-          <p className="text-textPrimary">o nas</p>
-          <p className="text-textPrimary">cennik</p>
-          <p className="text-textPrimary">barberzy</p>
-          <p className="text-textPrimary">nasze prace</p>
-        </div>
-        <div className="flex justify-center absolute left-1/2 transform -translate-x-1/2">
-          <img src={ViteIcon} alt="Vite Icon" />
-        </div>
-        <div className="flex space-x-10 uppercase">
-          <p className="text-textPrimary">blog</p>
-          <p className="text-textPrimary">szkolenia</p>
-          <p className="text-textPrimary">zarezerwuj</p>
-        </div>
-      </nav>
-      <div className="flex flex-col items-start mt-60 leading-none max-w-screen-sm">
-        <h1 className="text-textPrimary uppercase text-titleOne">
-          mento barber
-        </h1>
-        <h1 className="text-titleTwo uppercase">bochnia</h1>
-        <p>
-          MENTO Barber Bochnia Shop to znakomite miejsce, gdzie rzemiosło
-          spotyka się z pasją i profesjonalizmem. Nasza oferta skupia się na
-          perfekcyjnych męskich fryzurach, precyzyjnym strzyżeniu brody, oraz
-          zadbanym zarostem.
-        </p>
-      </div>
-      <div className="flex justify-between items-center w-full mt-auto py-4">
-        <div className="flex space-x-4">
-          <div className="border border-gray-400 rounded-full p-2 text-textPrimary">
-            Nad Babicą 2, Bochnia
-          </div>
-          <div className="border border-gray-400 rounded-full p-2 text-textPrimary">
-            +48 123 456 789
-          </div>
-        </div>
-        <div className="flex space-x-4 text-textPrimary">
-          <div>Facebook</div>
-          <div>Instagram</div>
-          <div>TikTok</div>
-        </div>
-      </div>
-    </div>
-  );
+	const settings = {
+		dots: false,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		speed: 2000,
+		autoplaySpeed: 0,
+		cssEase: "linear",
+		arrows: false,
+	};
+
+	const reverseSettings = {
+		...settings,
+		rtl: true,
+	};
+
+	return (
+		<div className='h-screen overflow-y-scroll snap-y snap-mandatory'>
+			<Navbar />
+			{/* Pierwszy ekran */}
+			<div
+				id='about'
+				className='flex flex-col min-h-screen px-20 pt-12 snap-start'>
+				<div className='flex flex-col items-start mt-60 leading-none max-w-screen-sm'>
+					<h1 className='text-textPrimary uppercase text-titleOne'>
+						mento barber
+					</h1>
+					<h1 className='text-titleTwo uppercase'>bochnia</h1>
+					<p>
+						MENTO Barber Bochnia Shop to znakomite miejsce, gdzie rzemiosło
+						spotyka się z pasją i profesjonalizmem. Nasza oferta skupia się na
+						perfekcyjnych męskich fryzurach, precyzyjnym strzyżeniu brody, oraz
+						zadbanym zarostem.
+					</p>
+				</div>
+				<div className='flex justify-between items-center w-full mt-auto py-4'>
+					<div className='flex space-x-4'>
+						<div className='border border-gray-400 rounded-full p-2 text-textPrimary'>
+							Nad Babicą 2, Bochnia
+						</div>
+						<div className='border border-gray-400 rounded-full p-2 text-textPrimary'>
+							+48 123 456 789
+						</div>
+					</div>
+					<div className='flex space-x-4 text-textPrimary'>
+						<div>Facebook</div>
+						<div>Instagram</div>
+						<div>TikTok</div>
+					</div>
+				</div>
+			</div>
+			{/* Drugi ekran - cennik */}
+			<div
+				id='pricing'
+				className='h-screen flex items-center justify-center bg-black text-white snap-start'>
+				<div className='grid grid-cols-2 gap-6 p-10 border border-orange-500'>
+					<div className='border p-4'>STRZYŻENIE MĘSKIE - 80 PLN | 30 min</div>
+					<div className='border p-4'>WŁOSY + BRODA - 130 PLN | 60 min</div>
+					<div className='border p-4'>STRZYŻENIE BRODY - 60 PLN | 30 min</div>
+					<div className='border p-4'>
+						BRODA + ODSIWIANIE - 130 PLN | 45 min
+					</div>
+				</div>
+			</div>
+			{/* Trzeci ekran - barberzy */}
+			<div
+				id='barbers'
+				className='h-screen flex items-center justify-center bg-darkBlue text-white snap-start'>
+				<div className='text-center p-10'>
+					<h1 className='text-6xl font-bold text-orange-500'>JIMMY</h1>
+					<p className='mt-4 max-w-lg'>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at
+						sollicitudin libero, vitae eleifend ante. Donec quis tellus
+						bibendum, tincidunt massa id, euismod nisl.
+					</p>
+				</div>
+			</div>
+			{/* Czwarty ekran - nasze prace */}
+			<div
+				id='works'
+				className='h-screen flex items-center justify-center bg-black text-white snap-start'>
+				<div className='text-center w-full'>
+					<div className='w-full'>
+						<Slider {...settings}>
+							<div className='bg-red-500 p-4'>
+								<h3>1</h3>
+							</div>
+							<div className='bg-green-500 p-4'>
+								<h3>2</h3>
+							</div>
+							<div className='bg-blue-500 p-4'>
+								<h3>3</h3>
+							</div>
+							<div className='bg-yellow-500 p-4'>
+								<h3>4</h3>
+							</div>
+							<div className='bg-purple-500 p-4'>
+								<h3>5</h3>
+							</div>
+							<div className='bg-pink-500 p-4'>
+								<h3>6</h3>
+							</div>
+						</Slider>
+					</div>
+					<div className='w-full mt-10'>
+						<Slider {...reverseSettings}>
+							<div className='bg-red-500 p-4'>
+								<h3>1</h3>
+							</div>
+							<div className='bg-green-500 p-4'>
+								<h3>2</h3>
+							</div>
+							<div className='bg-blue-500 p-4'>
+								<h3>3</h3>
+							</div>
+							<div className='bg-yellow-500 p-4'>
+								<h3>4</h3>
+							</div>
+							<div className='bg-purple-500 p-4'>
+								<h3>5</h3>
+							</div>
+							<div className='bg-pink-500 p-4'>
+								<h3>6</h3>
+							</div>
+						</Slider>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
