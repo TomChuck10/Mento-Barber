120e91308b76983d79364413b58dc87b70bcfc55
TomChuck <m.tomczyk@gymmanager.io>
2025-04-03 17:29:01 +0200

    Added animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,150 +4,156 @@ import Logo from "../assets/mento_logo.png";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-	const [activeSection, setActiveSection] = useState("about");
-	const location = useLocation();
-	const navigate = useNavigate(); // Initialize navigate
+  const [activeSection, setActiveSection] = useState("about");
+  const location = useLocation();
+  const navigate = useNavigate(); // Initialize navigate
 
-	const scrollToSection = sectionId => {
-		if (location.pathname !== "/") {
-			navigate("/", { state: { scrollTo: sectionId } }); // Przekazanie sekcji w stanie nawigacji
-		} else {
-			scrollAndSetActive(sectionId);
-		}
-	};
+  const scrollToSection = (sectionId) => {
+    if (location.pathname !== "/") {
+      navigate("/", { state: { scrollTo: sectionId } }); // Przekazanie sekcji w stanie nawigacji
+    } else {
+      scrollAndSetActive(sectionId);
+    }
+  };
 
-	const scrollAndSetActive = sectionId => {
-		setTimeout(() => {
-			const element = document.getElementById(sectionId);
-			if (element) {
-				element.scrollIntoView({ behavior: "smooth" });
-				setActiveSection(sectionId);
-			}
-		}, 100);
-	};
+  const scrollAndSetActive = (sectionId) => {
+    setTimeout(() => {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+        setActiveSection(sectionId);
+      }
+    }, 100);
+  };
 
-	useEffect(() => {
-		if (location.state?.scrollTo) {
-			scrollAndSetActive(location.state.scrollTo);
-		}
-	}, [location]);
+  useEffect(() => {
+    if (location.state?.scrollTo) {
+      scrollAndSetActive(location.state.scrollTo);
+    }
+  }, [location]);
 
-	useEffect(() => {
-		if (
-			location.pathname === "/training" ||
-			location.pathname === "/blog" ||
-			location.pathname === "/blog/boy-haircut"
-		) {
-			setActiveSection("");
-		}
-	}, [location.pathname]);
+  useEffect(() => {
+    if (
+      location.pathname === "/training" ||
+      location.pathname === "/blog" ||
+      location.pathname === "/blog/boy-haircut"
+    ) {
+      setActiveSection("");
+    }
+  }, [location.pathname]);
 
-	useEffect(() => {
-		const handleScroll = () => {
-			const sections = ["about", "pricing", "barbers", "works"];
-			const scrollPosition = window.scrollY + window.innerHeight / 2;
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = ["about", "pricing", "barbers", "works"];
+      const scrollPosition = window.scrollY + window.innerHeight / 2;
 
-			for (let section of sections) {
-				const element = document.getElementById(section);
-				if (
-					element &&
-					element.offsetTop <= scrollPosition &&
-					element.offsetTop + element.offsetHeight > scrollPosition
-				) {
-					setActiveSection(section);
-					break;
-				}
-			}
-		};
+      for (let section of sections) {
+        const element = document.getElementById(section);
+        if (
+          element &&
+          element.offsetTop <= scrollPosition &&
+          element.offsetTop + element.offsetHeight > scrollPosition
+        ) {
+          setActiveSection(section);
+          break;
+        }
+      }
+    };
 
-		window.addEventListener("scroll", handleScroll);
-		return () => {
-			window.removeEventListener("scroll", handleScroll);
-		};
-	}, []);
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-	return (
-		<nav className='flex justify-between items-center w-full fixed top-10 left-0 bg-transparent px-[98px] py-4 z-50'>
-			<div className='flex space-x-[32px] uppercase text-lg font-light'>
-				<p
-					className={`cursor-pointer flex items-center ${
-						activeSection === "about" ? "text-prime" : "text-textPrimary"
-					}`}
-					onClick={() => {
-						scrollToSection("about");
-					}}>
-					{activeSection === "about" && (
-						<span className='w-2 h-2 bg-prime rounded-full mr-2'></span>
-					)}
-					o nas
-				</p>
-				<p
-					className={`cursor-pointer flex items-center ${
-						activeSection === "pricing" ? "text-prime" : "text-textPrimary"
-					}`}
-					onClick={() => scrollToSection("pricing")}>
-					{activeSection === "pricing" && (
-						<span className='w-2 h-2 bg-prime rounded-full mr-2'></span>
-					)}
-					cennik
-				</p>
-				<p
-					className={`cursor-pointer flex items-center ${
-						activeSection === "barbers" ? "text-prime" : "text-textPrimary"
-					}`}
-					onClick={() => scrollToSection("barbers")}>
-					{activeSection === "barbers" && (
-						<span className='w-2 h-2 bg-prime rounded-full mr-2'></span>
-					)}
-					barberzy
-				</p>
-				<p
-					className={`cursor-pointer flex items-center ${
-						activeSection === "works" ? "text-prime" : "text-textPrimary"
-					}`}
-					onClick={() => scrollToSection("works")}>
-					{activeSection === "works" && (
-						<span className='w-2 h-2 bg-prime rounded-full mr-2'></span>
-					)}
-					nasze prace
-				</p>
-			</div>
-			<div className='flex justify-center absolute left-1/2 transform -translate-x-1/2'>
-				<Link to='/'>
-					<img src={Logo} alt='Vite Icon' className='logo cursor-pointer' />
-				</Link>
-			</div>
-			<div className='flex space-x-[32px] uppercase text-lg font-light'>
-				<Link to='/blog'>
-					{location.pathname === "/blog" ||
-					location.pathname === "/blog/boy-haircut" ? (
-						<p
-							className='cursor-pointer flex items-center text-prime
-						'>
-							<span className='w-2 h-2 bg-prime rounded-full mr-2'></span>
-							blog
-						</p>
-					) : (
-						<p className='text-textPrimary cursor-pointer'>blog</p>
-					)}
-				</Link>
-				<Link to='/training'>
-					{location.pathname === "/training" ? (
-						<p
-							className='cursor-pointer flex items-center text-prime
-						'>
-							<span className='w-2 h-2 bg-prime rounded-full mr-2'></span>
-							szkolenia
-						</p>
-					) : (
-						<p className='text-textPrimary cursor-pointer'>szkolenia</p>
-					)}
-				</Link>
-				<p className='text-prime cursor-pointer flex items-center gap-2'>
-					<span>{`>`}</span> zarezerwuj <span>{`<`}</span>
-				</p>
-			</div>
-			<style>{`
+  return (
+    <nav className="flex justify-between items-center w-full fixed top-10 left-0 bg-transparent px-[98px] py-4 z-50">
+      <div className="flex space-x-[32px] uppercase text-lg font-light">
+        <p
+          className={`cursor-pointer flex items-center ${
+            activeSection === "about" ? "text-prime" : "text-textPrimary"
+          }`}
+          onClick={() => {
+            scrollToSection("about");
+          }}
+        >
+          {activeSection === "about" && (
+            <span className="w-2 h-2 bg-prime rounded-full mr-2"></span>
+          )}
+          o nas
+        </p>
+        <p
+          className={`cursor-pointer flex items-center ${
+            activeSection === "pricing" ? "text-prime" : "text-textPrimary"
+          }`}
+          onClick={() => scrollToSection("pricing")}
+        >
+          {activeSection === "pricing" && (
+            <span className="w-2 h-2 bg-prime rounded-full mr-2"></span>
+          )}
+          cennik
+        </p>
+        <p
+          className={`cursor-pointer flex items-center ${
+            activeSection === "barbers" ? "text-prime" : "text-textPrimary"
+          }`}
+          onClick={() => scrollToSection("barbers")}
+        >
+          {activeSection === "barbers" && (
+            <span className="w-2 h-2 bg-prime rounded-full mr-2"></span>
+          )}
+          barberzy
+        </p>
+        <p
+          className={`cursor-pointer flex items-center ${
+            activeSection === "works" ? "text-prime" : "text-textPrimary"
+          }`}
+          onClick={() => scrollToSection("works")}
+        >
+          {activeSection === "works" && (
+            <span className="w-2 h-2 bg-prime rounded-full mr-2"></span>
+          )}
+          nasze prace
+        </p>
+      </div>
+      <div className="flex justify-center absolute left-1/2 transform -translate-x-1/2">
+        <Link to="/">
+          <img src={Logo} alt="Vite Icon" className="logo cursor-pointer" />
+        </Link>
+      </div>
+      <div className="flex space-x-[32px] uppercase text-lg font-light">
+        <Link to="/blog">
+          {location.pathname === "/blog" ||
+          location.pathname === "/blog/boy-haircut" ? (
+            <p
+              className="cursor-pointer flex items-center text-prime
+						"
+            >
+              <span className="w-2 h-2 bg-prime rounded-full mr-2"></span>
+              blog
+            </p>
+          ) : (
+            <p className="text-textPrimary cursor-pointer">blog</p>
+          )}
+        </Link>
+        <Link to="/training">
+          {location.pathname === "/training" ? (
+            <p
+              className="cursor-pointer flex items-center text-prime
+						"
+            >
+              <span className="w-2 h-2 bg-prime rounded-full mr-2"></span>
+              szkolenia
+            </p>
+          ) : (
+            <p className="text-textPrimary cursor-pointer">szkolenia</p>
+          )}
+        </Link>
+        <p className="text-prime cursor-pointer flex items-center gap-2">
+          <span>{`>`}</span> zarezerwuj <span>{`<`}</span>
+        </p>
+      </div>
+      <style>{`
 				.logo {
 					height: 80px;
 					margin-top: -10px;
@@ -159,8 +165,8 @@ const Navbar = () => {
 					}
 				}
 			`}</style>
-		</nav>
-	);
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/pages/Training.jsx
+++ src/pages/Training.jsx
@@ -11,103 +11,108 @@ import LongHair from "../assets/longHair.png";
 import Beard from "../assets/beard.png";
 
 const Training = () => {
-	const [activePanel, setActivePanel] = useState(0);
+  const [activePanel, setActivePanel] = useState(0);
 
-	const data = [
-		{
-			photo: ShortHair,
-			title: "Krótkie włosy + cieniowanie od zera (fade)",
-			description:
-				"Perfekcyjny fade to podstawa nowoczesnego barberingu. W tym panelu skupimy się na technice cieniowania od zera, precyzyjnym przejściu tonalnym i odpowiednim doborze długości do kształtu głowy. Pokażemy, jak pracować z maszynką, trymerem i shaverem, by uzyskać idealne wykończenie. Szkolenie obejmuje zarówno teorię, jak i praktykę na modelach, abyś mógł doszlifować swoje umiejętności i zyskać pewność w pracy z krótkimi fryzurami.",
-		},
-		{
-			photo: LongHair,
-			title: "Długie włosy , praca na nożyczkach",
-			description:
-				"Precyzyjna praca nożyczkami to podstawa przy dłuższych fryzurach. W tym panelu przepracujemy główne techniki strzyżenia, takie jak linia , gradacja , warstwa , aby dopasować fryzurę do kształtu głowy i struktury włosa. Nauczysz się, jak kontrolować napięcie pasma, jak pracować sekcyjnie i uzyskiwać płynne przejścia. Skupimy się na ergonomii ruchu oraz doborze narzędzi, byś mógł świadomie budować formę każdej fryzury.",
-		},
-		{
-			photo: Beard,
-			title: "Krótkie włosy , praca nmaszynką",
-			description:
-				"Idealnie przycięta broda to nie tylko estetyka, ale i komfort klienta. W tym panelu nauczysz się cieniowania brody, jej konturowania i odpowiedniego skracania, by dopasować kształt do rysów twarzy. Skupimy się także na tonowaniu i pielęgnacji zarostu, aby uzyskać naturalny, zdrowy wygląd. Podczas praktycznej części dowiesz się, jak pracować maszynka , trymerem, brzytwą i nożyczkami, by broda wyglądała perfekcyjnie i była dopasowana do stylu klienta.",
-		},
-	];
+  const data = [
+    {
+      photo: ShortHair,
+      title: "Krótkie włosy + cieniowanie od zera (fade)",
+      description:
+        "Perfekcyjny fade to podstawa nowoczesnego barberingu. W tym panelu skupimy się na technice cieniowania od zera, precyzyjnym przejściu tonalnym i odpowiednim doborze długości do kształtu głowy. Pokażemy, jak pracować z maszynką, trymerem i shaverem, by uzyskać idealne wykończenie. Szkolenie obejmuje zarówno teorię, jak i praktykę na modelach, abyś mógł doszlifować swoje umiejętności i zyskać pewność w pracy z krótkimi fryzurami.",
+    },
+    {
+      photo: LongHair,
+      title: "Długie włosy , praca na nożyczkach",
+      description:
+        "Precyzyjna praca nożyczkami to podstawa przy dłuższych fryzurach. W tym panelu przepracujemy główne techniki strzyżenia, takie jak linia , gradacja , warstwa , aby dopasować fryzurę do kształtu głowy i struktury włosa. Nauczysz się, jak kontrolować napięcie pasma, jak pracować sekcyjnie i uzyskiwać płynne przejścia. Skupimy się na ergonomii ruchu oraz doborze narzędzi, byś mógł świadomie budować formę każdej fryzury.",
+    },
+    {
+      photo: Beard,
+      title: "Krótkie włosy , praca nmaszynką",
+      description:
+        "Idealnie przycięta broda to nie tylko estetyka, ale i komfort klienta. W tym panelu nauczysz się cieniowania brody, jej konturowania i odpowiedniego skracania, by dopasować kształt do rysów twarzy. Skupimy się także na tonowaniu i pielęgnacji zarostu, aby uzyskać naturalny, zdrowy wygląd. Podczas praktycznej części dowiesz się, jak pracować maszynka , trymerem, brzytwą i nożyczkami, by broda wyglądała perfekcyjnie i była dopasowana do stylu klienta.",
+    },
+  ];
 
-	const handleChangePanel = index => setActivePanel(index);
+  const handleChangePanel = (index) => setActivePanel(index);
 
-	const renderPanel = (panel, index) => (
-		<div
-			key={index}
-			className={`h-[33.3%] pl-[36px] border-l-4 ${
-				activePanel === index ? "border-prime" : "border-transparent"
-			} border-b-[1px] flex flex-col justify-center cursor-pointer`}
-			style={{
-				borderBottomColor:
-					activePanel === index
-						? "rgba(255, 255, 255, 0.1)"
-						: "rgba(112,112,112, 0.1)",
-			}}
-			onClick={() => handleChangePanel(index)}>
-			<p className='text-white uppercase text-[14px]'>panel {index + 1}</p>
-			<p
-				className={`text-[18px] ${
-					activePanel === index ? "text-prime" : "text-white"
-				}`}>
-				{panel.title}
-			</p>
-		</div>
-	);
+  const renderPanel = (panel, index) => (
+    <div
+      key={index}
+      className={`h-[33.3%] pl-[36px] border-l-4 ${
+        activePanel === index ? "border-prime" : "border-transparent"
+      } border-b-[1px] flex flex-col justify-center cursor-pointer`}
+      style={{
+        borderBottomColor:
+          activePanel === index
+            ? "rgba(255, 255, 255, 0.1)"
+            : "rgba(112,112,112, 0.1)",
+      }}
+      onClick={() => handleChangePanel(index)}
+    >
+      <p className="text-white uppercase text-[14px]">panel {index + 1}</p>
+      <p
+        className={`text-[18px] ${
+          activePanel === index ? "text-prime" : "text-white"
+        }`}
+      >
+        {panel.title}
+      </p>
+    </div>
+  );
 
-	return (
-		<div className='h-screen '>
-			<Navbar />
-			<div
-				className='relative min-h-screen h-screen overflow-hidden'
-				style={{
-					background: `linear-gradient(90deg, #090909, #091E23), url(${Texture})`,
-					backgroundSize: "cover",
-					backgroundRepeat: "no-repeat",
-					backgroundPosition: "center",
-					backgroundBlendMode: "multiply",
-				}}>
-				<div className='absolute w-full flex items-center justify-center h-full z-[2]'>
-					<div className='flex items-center justify-center gap-[16px] w-[calc(100%-196px)] mt-[100px]'>
-						<div
-							className='h-[75vh] w-[33%] bg-[#171D1F]'
-							style={{ boxShadow: "0px 8px 10px #00000099" }}>
-							{data.map((panel, index) => renderPanel(panel, index))}
-						</div>
-						<div
-							className='h-[75vh] w-[66%] bg-[#171D1F] flex flex-col'
-							style={{ boxShadow: "0px 8px 10px #00000099" }}>
-							<div className=' bg-white m-[32px] mb-[20px] rounded-[6px] overflow-hidden'>
-								<img
-									src={data[activePanel].photo}
-									alt='photo'
-									className='w-full h-full object-cover'
-								/>
-							</div>
-							<p className='title text-prime mx-[32px] text-[20px]'>
-								{data[activePanel].title}
-							</p>
-							<p className='description text-[13px] text-white mx-[32px] mt-[10px] mb-[24px] font-light'>
-								{data[activePanel].description}
-							</p>
-							<button className='button border-2 border-prime rounded-[16px] mx-[32px] p-[12px] text-prime mb-[36px] mt-auto'>
-								Umów szkolenie
-							</button>
-						</div>
-					</div>
-				</div>
-				<img
-					src={Gradient}
-					alt='Gradient'
-					className='absolute w-full h-auto pointer-events-none z-[1] opacity-70'
-					style={{ top: "-50%" }}
-				/>
-			</div>
-			<style>{`
+  return (
+    <div className="h-screen ">
+      <Navbar />
+      <div
+        className="relative min-h-screen h-screen overflow-hidden"
+        style={{
+          background: `linear-gradient(90deg, #090909, #091E23), url(${Texture})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundBlendMode: "multiply",
+        }}
+      >
+        <div className="absolute w-full flex items-center justify-center h-full z-[2]">
+          <div className="flex items-center justify-center gap-[16px] w-[calc(100%-196px)] mt-[100px]">
+            <div
+              className="h-[75vh] w-[33%] bg-[#171D1F]"
+              style={{ boxShadow: "0px 8px 10px #00000099" }}
+            >
+              {data.map((panel, index) => renderPanel(panel, index))}
+            </div>
+            <div
+              className="h-[75vh] w-[66%] bg-[#171D1F] flex flex-col"
+              style={{ boxShadow: "0px 8px 10px #00000099" }}
+            >
+              <div className=" bg-white m-[32px] mb-[20px] rounded-[6px] overflow-hidden">
+                <img
+                  src={data[activePanel].photo}
+                  alt="photo"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <p className="title text-prime mx-[32px] text-[20px]">
+                {data[activePanel].title}
+              </p>
+              <p className="description text-[13px] text-white mx-[32px] mt-[10px] mb-[24px] font-light">
+                {data[activePanel].description}
+              </p>
+              <button className="button border-2 border-prime rounded-[16px] mx-[32px] p-[12px] text-prime mb-[36px] mt-auto">
+                Umów szkolenie
+              </button>
+            </div>
+          </div>
+        </div>
+        <img
+          src={Gradient}
+          alt="Gradient"
+          className="absolute w-full h-auto pointer-events-none z-[1] opacity-70"
+          style={{ top: "-50%" }}
+        />
+      </div>
+      <style>{`
 				@media (min-width: 1800px) {
 					.title {
 						font-size: 24px;
@@ -120,8 +125,8 @@ const Training = () => {
 					}
 				}
 			`}</style>
-		</div>
-	);
+    </div>
+  );
 };
 
 export default Training;
